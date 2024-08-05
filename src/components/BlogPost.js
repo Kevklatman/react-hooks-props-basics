4904b2de5324e11396703778d82d2c53e8f5f0a6
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-04 19:22:57 -0600

    darth plagueis

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -4,11 +4,14 @@ import Comment from "./Comment";
 
 function BlogPost() {
   return (
-    <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+    <div id="blog-post" >
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." isPublished={true} />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <div style={{backgroundColor: "red", fontSize: "98px", fontStyle: "italic", fontWeight: "bolder" }}>
+        <Comment commentText="Did you ever hear the tragedy of Darth Plagueis the wise?"/>
+      </div>
     </div>
   );
 }
